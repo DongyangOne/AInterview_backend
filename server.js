3b7d0eae3550f043dc5a99d8dec6ff8f6ab314dc
Dragons0up <jungyongguk5@gmail.com>
2025-07-06 14:41:16 +0900

    feat: 알림 조회
    - promise 미사용으로 callback 방식으로 처리함

--- server.js
+++ server.js
@@ -4,9 +4,11 @@ const app = express();
 app.use(express.json());
 
 const Router = require('./routes/basicRouter');
+const noticeRouter = require('./routes/noticeRouter');
 const db = require('./config/database');
 
 app.use('/example', Router);
+app.use('/notice', noticeRouter);
 
 const port = process.env.s_port || 3000;
 app.listen(port, '0.0.0.0', () => {
