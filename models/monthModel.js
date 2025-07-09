f7da181a027a5b29cb4c9e7d6cad88d870659f0c
chldPDms <115327796+chldPDms@users.noreply.github.com>
2025-07-10 01:58:15 +0900

    backend-15 해당 달 조회

---
+++ models/monthModel.js
@@ -0,0 +1,16 @@
+const db = require('../config/database');
+
+const getUserMonth = (userId, year, month, callback) => {
+    const sql = `select \`year\` , date_format(time, '%m'), calendar_id, title
+    from calendar where users_id = ? and \`year\` = ? and MONTH(time) = ?
+    order by created_at desc`;
+
+    db.query(sql, [userId, year, month], (err, results) => {
+        if (err) return callback(err);
+        callback(null, results);
+    });
+};
+
+module.exports = {
+    getUserMonth
+};
--- server.js
+++ server.js
@@ -3,10 +3,17 @@ const express = require('express');
 const app = express();
 app.use(express.json());
 
-const Router = require('./routes/basicRouter');
+const noticeRouter = require('./routes/noticeRouter');
+const signRouter = require('./routes/signRouter');
+const logoutRouter = require('./routes/logoutRouter');
+const monthRouter = require('./routes/monthRouter');
 const db = require('./config/database');
 
 app.use('/example', Router);
+app.use('/notice', noticeRouter);
+app.use('/sign', signRouter);
+app.use('/logout', logoutRouter);
+app.use('/calendar', monthRouter);
 
 const port = process.env.s_port || 3000;
 app.listen(port, '0.0.0.0', () => {
