fd08b539081c1baa47d5796e293729c422e9c0e9
chldPDms <115327796+chldPDms@users.noreply.github.com>
2025-07-11 03:09:43 +0900

    backend-17/18/19

---
+++ models/calendarModel.js
@@ -0,0 +1,62 @@
+const db = require('../config/database');
+
+const getCalendarAdd = (userId, year, title, time, importance, memo, callback) => {
+    const sql = `insert into calendar (users_id, year, title, time, importance, memo)
+    values (?, ?, ?, ?, ?, ?)`;
+
+    db.query(sql, [userId, year, title, time, importance, memo], (err, results) => {
+        if (err) return callback(err);
+        callback(null, results);
+    });
+};
+
+const getCalendarDelete = (userId, callback) => {
+    const sql = `delete from calendar where users_id = ?`;
+    db.query(sql, [userId], (err, results) => {
+        if (err) return callback(err);
+        callback(null, results);
+    });
+};
+
+const getCalendarUpdate = (userId, year, title, time, importance, memo, callback) => { //모든 컬럼을 한 번에 수정하지 않고 하나하나 컬럼을 수정할 수 있게 함
+    let sql = `update calendar set `;
+    const arr = [], values = [];
+
+    if (year !== undefined){
+        arr.push(`year = ?`);
+        values.push(year);
+    }
+
+    if (title !== undefined){
+        arr.push(`title = ?`);
+        values.push(title);
+    }
+
+    if (time !== undefined){
+        arr.push(`time = ?`);
+        values.push(time);
+    }
+
+    if (importance !== undefined){
+        arr.push(`importance = ?`);
+        values.push(importance);
+    }
+
+    if (memo !== undefined){
+        arr.push(`memo = ?`);
+        values.push(memo);
+    }
+
+    sql += arr.join(`, `) + `where users_id = ?`;
+    values.push(userId);
+
+    db.query(sql, values, (err, results) => {
+        if (err) return callback(err);
+        callback(null, results);
+    });
+};
+
+
+module.exports = {
+    getCalendarAdd, getCalendarDelete, getCalendarUpdate
+};
