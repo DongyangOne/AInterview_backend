32f58d81addb1aadee0e4d01eeecee64615c6e2b
chldPDms <115327796+chldPDms@users.noreply.github.com>
2025-07-11 03:04:36 +0900

    backend-16 일정 상세 조회

---
+++ models/dayModel.js
@@ -0,0 +1,26 @@
+const db = require('../config/database');
+
+const getUserDay = (userId, year, month, day, callback) => {
+    const sql = `select calendar_id, title, DATE_FORMAT(time, '%Y-%m-%d') AS 날짜,
+    CASE DAYOFWEEK(time)
+        WHEN 1 THEN '일'
+        WHEN 2 THEN '월'
+        WHEN 3 THEN '화'
+        WHEN 4 THEN '수'
+        WHEN 5 THEN '목'
+        WHEN 6 THEN '금'
+        WHEN 7 THEN '토' END as 요일, importance, memo 
+    from calendar where users_id = ? and \`year\` = ? and MONTH(time) = ? and DAY(time) =?
+    order by created_at desc`;
+
+    db.query(sql, [userId, year, month, day], (err, results) => {
+        if (err) return callback(err);
+        callback(null, results);
+    console.log(results);
+
+    });
+};
+
+module.exports = {
+    getUserDay
+}; //MODEL
