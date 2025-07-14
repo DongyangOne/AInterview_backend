17cacb67226dd5070e8d9a676ecff8860eccfef9
marianddi <hanna4331@naver.com>
2025-07-15 05:25:42 +0900

    backend-24
    비밀번호 일치 확인 기능 구현

--- routes/userRouter.js
+++ routes/userRouter.js
@@ -1,9 +1,11 @@
 const express = require('express');
 const router = express.Router();
-const {pwChange, nicknameChange, setAppPush} = require('../controllers/mypage/userController');
+const {pwChange, nicknameChange, setAppPush, passwordCheck} = require('../controllers/mypage/userController');
 
 router.get('/changePw', pwChange);
 router.get('/changeName', nicknameChange);
 router.get('/setAppPush', setAppPush);
+router.get('/checkPw', passwordCheck);
+
 
 module.exports = router;
