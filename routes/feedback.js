cff8b39a91e2d39296d26b6c853bd72f8ef1c7cc
msh1234777 <your@email.com>
2025-07-15 17:11:36 +0900

    backend_10

--- routes/feedback.js
+++ routes/feedback.js
@@ -2,7 +2,7 @@ const express = require('express');
 const router = express.Router();
 const { sortFeedbacks } = require('../controllers/feedbacks/sort');
 
-// 피드백 정렬
+//  피드백 정렬
 router.get('/sort', sortFeedbacks);
 
 module.exports = router;
