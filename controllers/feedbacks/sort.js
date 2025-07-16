01372b35f1cdbd9d7578096889299444ed308e18
msh1234777 <your@email.com>
2025-07-16 20:24:10 +0900

    피드백 정렬

--- controllers/feedbacks/sort.js
+++ controllers/feedbacks/sort.js
@@ -1,10 +1,13 @@
 const db = require('../../config/database');
 
-// 피드백 정렬 (날짜순, 가나다순)
+// 피드백 정렬 (날짜별 정렬, 가나다별 정렬)
 exports.sortFeedbacks = (req, res) => {
   const { by } = req.query; 
 
+  // 날짜별 정렬: 최신순
   let orderBy = 'date DESC';
+
+  // 가나다별 정렬: 제목 오름차순
   if (by === 'alpha') orderBy = 'title ASC';
 
   const sql = `SELECT * FROM feedbacks ORDER BY is_pinned DESC, ${orderBy}`;
