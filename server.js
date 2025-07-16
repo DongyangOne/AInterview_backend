a9973f4ac5c075652e65923dc7b5f6b091b10927
msh1234777 <your@email.com>
2025-07-17 00:41:17 +0900

    피드백 고정

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const session = require('express-session');
 const app = express();
 app.use(express.json());
 app.use(session({
-  secret: 'yourSecretKey', //세션 보호용 키
+  secret: 'yourSecretKey', 
   resave: false,
   saveUninitialized: true,
   cookie: { secure: false }
@@ -14,14 +14,16 @@ const Router = require('./routes/basicRouter');
 const noticeRouter = require('./routes/noticeRouter');
 const signRouter = require('./routes/signRouter');
 const logoutRouter = require('./routes/logoutRouter');
-const db = require('./config/database');
+const feedbackRouter = require('./routes/feedback'); 
 
 app.use('/example', Router);
 app.use('/notice', noticeRouter);
 app.use('/sign', signRouter);
 app.use('/logout', logoutRouter);
 
-const port = process.env.s_port || 3000;
+app.use('/feedback', feedbackRouter);
+
+const port = process.env.S_PORT || 3000;
 app.listen(port, '0.0.0.0', () => {
   console.log(`서버 시작 ${port}`);
 });
