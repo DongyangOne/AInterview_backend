5e4631473449ae4c79058ab976ec6d0885f1babe
msh1234777 <your@email.com>
2025-07-17 00:12:44 +0900

    피드백 검색

--- server.js
+++ server.js
@@ -5,11 +5,12 @@ const app = express();
 
 app.use(express.json());
 app.use(session({
-  secret: 'yourSecretKey', // 세션 보호용 키
+  secret: 'yourSecretKey', 
   resave: false,
   saveUninitialized: true,
   cookie: { secure: false }
 }));
+
 
 const Router = require('./routes/basicRouter');
 const noticeRouter = require('./routes/noticeRouter');
@@ -22,9 +23,11 @@ app.use('/example', Router);
 app.use('/notice', noticeRouter);
 app.use('/sign', signRouter);
 app.use('/logout', logoutRouter);
-app.use('/search', feedbackRouter);  
 
-const port = process.env.s_port || 3000;
+
+app.use('/', feedbackRouter);
+
+const port = process.env.S_PORT || 3000;
 app.listen(port, '0.0.0.0', () => {
   console.log(`서버 시작 ${port}`);
 });
