1fae5f6d332288345eeddfd2a41c4a8e8ca3806b
chldPDms <115327796+chldPDms@users.noreply.github.com>
2025-07-19 02:06:41 +0900

    Revert "피드백 고정"
    This reverts commit a7fa886e2885c9701a8ffe6d6d6109ef13bfd59f.

--- config/database.js
+++ config/database.js
@@ -2,11 +2,19 @@ require('dotenv').config();
 const mysql = require("mysql2");
 
 const dbInfo = {
+<<<<<<< HEAD
    host: process.env.db_host, //183.101.17.181으로 작성
    port: process.env.db_port,
    user: process.env.db_user, //이름 이니셜대로 작성
    password: process.env.db_pw, 
    database: process.env.db,
+=======
+   host: process.env.DB_HOST,
+   port: process.env.DB_PORT,
+   user: process.env.DB_USER,
+   password: process.env.DB_PASSWORD,
+   database: process.env.DB_NAME,
+>>>>>>> parent of a7fa886 (피드백 고정)
 };
 
 const connection = mysql.createConnection(dbInfo);
