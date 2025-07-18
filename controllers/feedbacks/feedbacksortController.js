ce3cd3e932c2d6e9a6e880c978cd00bae8b1d8b4
msh1234777 <your@email.com>
2025-07-18 21:55:19 +0900

    피드백 정 렬

--- controllers/feedbacks/feedbacksortController.js
+++ controllers/feedbacks/feedbacksortController.js
@@ -2,7 +2,15 @@ const FeedbackModel = require('../../models/sortfeedbackModel');
 
 exports.sortFeedbacks = (req, res) => {
   const { by } = req.query;
-  FeedbackModel.sortFeedbacks(by, (err, rows) => {
+  
+  let orderBy;
+  if (by === 'alpha') {
+    orderBy = 'title ASC';      // 가나다순
+  } else {
+    orderBy = 'date DESC';      // 날짜순
+  }
+
+  FeedbackModel.sortFeedbacks(orderBy, (err, rows) => {
     if (err) {
       console.error('피드백 정렬 오류:', err);
       return res.status(500).json({ error: err.message });
--- models/sortfeedbackModel.js
+++ models/sortfeedbackModel.js
@@ -1,8 +1,6 @@
 const db = require('../config/database');
 
-exports.sortFeedbacks = (by, callback) => {
-  let orderBy = 'date DESC';
-  if (by === 'alpha') orderBy = 'title ASC';
+exports.sortFeedbacks = (orderBy, callback) => {
   const sql = `SELECT * FROM feedbacks ORDER BY is_pinned DESC, ${orderBy}`;
   db.query(sql, callback);
 };
