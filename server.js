82d77202e59748b7af3b81145603c68f96795a0b
chldPDms <115327796+chldPDms@users.noreply.github.com>
2025-07-20 04:50:18 +0900

    Revert "Merge pull request #26 from DongyangOne/backend3"
    This reverts commit a489bc0221d5360df1409e3195088e8f8b1cda20, reversing
    changes made to fa78235a8285d65f329e98f9375d903fd2dcacd2.

--- server.js
+++ server.js
@@ -14,19 +14,19 @@ const Router = require('./routes/basicRouter');
 const noticeRouter = require('./routes/noticeRouter');
 const signRouter = require('./routes/signRouter');
 const logoutRouter = require('./routes/logoutRouter');
-const feedbackRouter=require('./routes/recentfeedbackRouter');
-const calendarRouter = require('./routes/twcalendarRouter');
 const db = require('./config/database');
 
 app.use('/example', Router);
 app.use('/notice', noticeRouter);
 app.use('/sign', signRouter);
 app.use('/logout', logoutRouter);
-app.use('/feedback',feedbackRouter);
+
+
+const calendarRouter = require('./routes/twcalendarRouter');
 app.use('/calendar', calendarRouter);
 
 
-  
+
 
 
 
