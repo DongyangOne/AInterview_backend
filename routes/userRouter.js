7bcff1a69f7f2655a0c8e87c551657689b4d5a50
chldPDms <115327796+chldPDms@users.noreply.github.com>
2025-07-20 02:21:19 +0900

    Update userRouter.js

--- routes/userRouter.js
+++ routes/userRouter.js
@@ -2,7 +2,7 @@ const express = require('express');
 const router = express.Router();
 const {pwChange, nicknameChange} = require('../controllers/mypage/userController');
 
-router.get('/changePw', pwChange);
+router.post('/changePw', pwChange);
 router.post('/changeName', nicknameChange);
 
 module.exports = router;
