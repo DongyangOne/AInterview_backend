61fc13a7b3b8d3890850b93500e0d06c3b586310
chldPDms <115327796+chldPDms@users.noreply.github.com>
2025-07-20 02:26:59 +0900

    backend-23 수정

--- routes/userRouter.js
+++ routes/userRouter.js
@@ -2,8 +2,8 @@ const express = require('express');
 const router = express.Router();
 const {pwChange, nicknameChange, setAppPush} = require('../controllers/mypage/userController');
 
-router.get('/changePw', pwChange);
-router.get('/changeName', nicknameChange);
+router.post('/changePw', pwChange);
+router.post('/changeName', nicknameChange);
 router.get('/setAppPush', setAppPush);
 
 module.exports = router;
