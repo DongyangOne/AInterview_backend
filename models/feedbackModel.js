57e303cf7f984bc7fc6fc244ba7efa45d78364bd
chldPDms <115327796+chldPDms@users.noreply.github.com>
2025-07-20 00:38:02 +0900

    backend-7 리스트 조회 수정

--- models/feedbackModel.js
+++ models/feedbackModel.js
@@ -2,10 +2,10 @@ const connection = require('../config/database');
 
 exports.findAllByUserId = ({ userId }, callback) => {
   const sql = `
-    SELECT feedback_id AS id, title, memo, created_at, updated_at
+    SELECT feedback_id AS id, title, memo, created_at
     FROM feedback
     WHERE userId = ?
-    ORDER BY feedback_id DESC
+    ORDER BY created_at DESC
   `;
   connection.query(sql, [userId], (err, rows) => {
     if (err) return callback(err, null);
