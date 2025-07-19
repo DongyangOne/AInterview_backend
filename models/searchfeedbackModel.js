30902abc4b2b7b6aa5373fec44d73f44cc89169c
chldPDms <115327796+chldPDms@users.noreply.github.com>
2025-07-19 17:47:44 +0900

    Update searchfeedbackModel.js

--- models/searchfeedbackModel.js
+++ models/searchfeedbackModel.js
@@ -3,6 +3,6 @@ const db = require('../config/database');
 // 피드백 검색 
 exports.searchFeedbacks = (keyword, callback) => {
    const sql = `SELECT feedback_id, userId, COUNT(feedback_id) as \`조회된 피드백\`,
-  title, content, memo, pin, created_at FROM feedback WHERE title LIKE ? ORDER BY created_at DESC`;
+  title, memo, pin, created_at FROM feedback WHERE title LIKE ? ORDER BY created_at DESC`;
   db.query(sql, [`%${keyword}%`], callback);
 };
