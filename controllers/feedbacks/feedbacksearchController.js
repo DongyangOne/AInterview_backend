d5242435768ca30dcbb0eeac9dbdc8e7873d10ad
chldPDms <115327796+chldPDms@users.noreply.github.com>
2025-07-19 17:44:32 +0900

    Update feedbacksearchController.js

--- controllers/feedbacks/feedbacksearchController.js
+++ controllers/feedbacks/feedbacksearchController.js
@@ -2,11 +2,11 @@ const FeedbackModel = require('../../models/searchfeedbackModel');
 
 exports.searchFeedbacks = (req, res) => {
   const { keyword } = req.query;
-  FeedbackModel.searchFeedbacks(keyword, (err, rows) => {
+  FeedbackModel.searchFeedbacks(keyword, (err, result) => {
     if (err) {
       console.error('피드백 검색 오류:', err);
       return res.status(500).json({ error: err.message });
     }
-    res.json(rows);
+       res.status(200).json({ success: true, data: result });
   });
 };
