c11e605ffe17298c199816e11c046a0c9bac72b4
chldPDms <115327796+chldPDms@users.noreply.github.com>
2025-07-20 12:17:23 +0900

    Update server.js

--- server.js
+++ server.js
@@ -14,25 +14,16 @@ const Router = require('./routes/basicRouter');
 const noticeRouter = require('./routes/noticeRouter');
 const signRouter = require('./routes/signRouter');
 const logoutRouter = require('./routes/logoutRouter');
+const questionRouter=require('./routes/questionRouter');
 const db = require('./config/database');
 
 app.use('/example', Router);
 app.use('/notice', noticeRouter);
 app.use('/sign', signRouter);
 app.use('/logout', logoutRouter);
-
-
-const questionRouter=require('./routes/questionRouter');
 app.use('/question',questionRouter);
 
-
-
-
-
-
-
-
-const port =  3000;
+const port = process.env.s_port || 3000;
 app.listen(port, '0.0.0.0', () => {
   console.log(`서버 시작 ${port}`);
 });
