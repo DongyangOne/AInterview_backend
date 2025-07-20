50bef2159a05629810071c5d31e0ea4e96069649
chldPDms <115327796+chldPDms@users.noreply.github.com>
2025-07-20 11:59:32 +0900

    backend-3

--- server.js
+++ server.js
@@ -15,6 +15,7 @@ const noticeRouter = require('./routes/noticeRouter');
 const signRouter = require('./routes/signRouter');
 const logoutRouter = require('./routes/logoutRouter');
 const recentfeedbackRouter=require('./routes/recentfeedbackRouter');
+const calendarRouter = require('./routes/twcalendarRouter');
 const db = require('./config/database');
 
 app.use('/example', Router);
@@ -22,6 +23,7 @@ app.use('/notice', noticeRouter);
 app.use('/sign', signRouter);
 app.use('/logout', logoutRouter);
 app.use('/feedback', recentfeedbackRouter);
+app.use('/calendar', calendarRouter);
 
 const port = process.env.s_port || 3000;
 app.listen(port, '0.0.0.0', () => {
