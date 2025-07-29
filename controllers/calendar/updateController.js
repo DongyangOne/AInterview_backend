e4556388b43af7ae77f79c0528091551c25f0103
chldPDms <115327796+chldPDms@users.noreply.github.com>
2025-07-30 03:17:17 +0900

    Update updateController.js

--- controllers/calendar/updateController.js
+++ controllers/calendar/updateController.js
@@ -29,21 +29,12 @@ const getUpdate = (req, res) => {
 
 getCalendarUpdate(userId, title, time, importance, memo, (err, result) => {
     if (err){
-        switch (err.code){
-            case 'DB_ERROR' :
-                return res.status(500).json({success:false, message: '서버 오류 발생', details: err});
-            case 'INVALID_URL' :
-                return res.status(404).json({success:false, message:'url 입력 에러', details: err});
-             case 'UNAUTHORIZED':
-                return res.status(401).json({ success: false, message: '인증 정보 없음', details: err });
-            case 'FORBIDDEN':
-                return res.status(403).json({ success: false, message: '접근 권한 없음', details: err });
-        }
+        return res.status(500).json({success:false, message: '오류 발생', details: err});
+    }
 
     const importanceResult = importanceArr[importance]
 
         res.status(200).json({success: true, data: result, importanceResult});
-    }
 })
 };
 
