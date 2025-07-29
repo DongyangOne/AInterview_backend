f87e5e8ab77e3b64710e2d5e959f64b8300be967
chldPDms <115327796+chldPDms@users.noreply.github.com>
2025-07-30 01:18:26 +0900

    Update updateController.js

--- controllers/calendar/updateController.js
+++ controllers/calendar/updateController.js
@@ -2,7 +2,6 @@ const {getCalendarUpdate} = require('../../models/dateupdateModel')
 
 const getUpdate = (req, res) => {
     const userId = req.query.userId;
-    const year = Number(req.query.year);
     const title = req.query.title;
     const time = req.query.time;
     const importance = req.query.importance;
@@ -15,39 +14,36 @@ const getUpdate = (req, res) => {
         N: 'X'
     };
 
-
-    if (!userId){
+    if (!userId || !title || !time || !importance){
         return res.status(400).json({success: false, message: '아이디가 존재하지 않습니다.'});
-    }
-
-     if (!year){
-        return res.status(400).json({success: false, message: '년도가 입력되지 않았습니다.'});
-    }
-
-    if (!title){
-        return res.status(400).json({success: false, message: '이름이 입력되지 않았습니다.'});
-    }
-
-    if (!time){
-        return res.status(400).json({success: false, message: '시간이 입력되지 않았습니다.'});
-    }
-
-    if (importance === null){
-        return res.status(400).json({success: false, message: '중요도를 선택해 주십시오.'});
     }
 
     if (title.length > 8){
         return res.status(400).json({success: false, message: '8자 미만으로 입력해 주십시오.'})
     }
 
-getCalendarUpdate(userId, year, title, time, importance, memo, (err, result) => {
-    if (err){
-        return res.status(500).json({success:false, message: '오류 발생', details: err});
+    if(importance !== 'S' && importance !== 'I' && importance !== 'N'){
+        return res.status(400).json({success: false, message: '중요도에는 s, i, n만 입력 가능합니다.'})
     }
+
+
+getCalendarUpdate(userId, title, time, importance, memo, (err, result) => {
+    if (err){
+        switch (err.code){
+            case 'DB_ERROR' :
+                return res.status(500).json({success:false, message: '서버 오류 발생', details: err});
+            case 'INVALID_URL' :
+                return res.status(404).json({success:false, message:'url 입력 에러', details: err});
+             case 'UNAUTHORIZED':
+                return res.status(401).json({ success: false, message: '인증 정보 없음', details: err });
+            case 'FORBIDDEN':
+                return res.status(403).json({ success: false, message: '접근 권한 없음', details: err });
+        }
 
     const importanceResult = importanceArr[importance]
 
         res.status(200).json({success: true, data: result, importanceResult});
+    }
 })
 };
 
