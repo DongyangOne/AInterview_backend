c22cff7ce4769ed12ee8d62e520c5e1e50fefbd2
msh1234777 <msh1234777@naver.com>
2025-08-01 02:43:28 +0900

    피드백 정렬 기능 에러처리

--- controllers/feedback/feedbacksortController.js
+++ controllers/feedback/feedbacksortController.js
@@ -2,19 +2,38 @@ const FeedbackModel = require('../../models/sortfeedbackModel');
 
 exports.sortFeedbacks = (req, res) => {
   const { by } = req.query;
-  
+
+  if (!by) {
+    return res.status(400).json({
+      success: false,
+      message: "미입력 정보가 존재합니다 (by)"
+    });
+  }
+
   let orderBy;
   if (by === 'alpha') {
-    orderBy = 'title ASC';      // 가나다순
+    orderBy = 'title ASC';
+  } else if (by === 'date') {
+    orderBy = 'created_at DESC';
   } else {
-    orderBy = 'created_at DESC';      // 날짜순
+    return res.status(400).json({
+      success: false,
+      message: "정렬 기준이 올바르지 않습니다 (by: alpha | date)"
+    });
   }
 
   FeedbackModel.sortFeedbacks(orderBy, (err, result) => {
     if (err) {
       console.error('피드백 정렬 오류:', err);
-      return res.status(500).json({ error: err.message });
+      return res.status(500).json({
+        success: false,
+        message: "서버 오류",
+        error: err.message
+      });
     }
-     res.status(200).json({ success: true, data: result });
+    return res.status(200).json({
+      success: true,
+      data: result
+    });
   });
 };
