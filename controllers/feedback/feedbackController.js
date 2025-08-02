33c4811d9b8e23a7a5f57caaf94cab02c93dd79f
kim min hyeok <royalkim0707@gmail.com>
2025-08-02 16:47:26 +0900

    피드백 삭제 기능 수정

--- controllers/feedback/feedbackController.js
+++ controllers/feedback/feedbackController.js
@@ -212,6 +212,13 @@ deleteById({ feedbackId, userId }, (err, result) => {
       });
     }
 
+     if (result.affectedRows === 0) {
+      return res.status(404).json({
+        success: false,
+        message: '삭제할 피드백을 찾을 수 없거나 삭제 권한이 없습니다.'
+      });
+    }
+
     res.status(200).json({
       success: true,
       message: '피드백이 성공적으로 삭제되었습니다.'
