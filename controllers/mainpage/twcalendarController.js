3195673481e96f8402ac2144b7b021b9798f0e5e
rlaghldbs <rlaghldbs2@naver.com>
2025-08-02 21:22:57 +0900

    backend2

--- controllers/mainpage/twcalendarController.js
+++ controllers/mainpage/twcalendarController.js
@@ -3,10 +3,6 @@ const { TwTODO } = require('../../models/twcalendarModel');
 
 const Twcalendar = (req, res) => {
     const userId = req.query.userId;
-<<<<<<< HEAD
-=======
-
->>>>>>> 9c2e91bb333fc0a122ae57eabf324d215d3307f5
 
     if (!userId) {
         return res.status(401).json({ success: false, message: 'user_id가 필요합니다.' });
