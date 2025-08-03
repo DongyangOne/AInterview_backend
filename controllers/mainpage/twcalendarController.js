3603da234fcaee52ce553a141b9f3da4f8b7749e
chldPDms <115327796+chldPDms@users.noreply.github.com>
2025-08-03 16:29:58 +0900

    Revert "backend4"

--- controllers/mainpage/twcalendarController.js
+++ controllers/mainpage/twcalendarController.js
@@ -5,14 +5,14 @@ const Twcalendar = (req, res) => {
     const userId = req.query.userId;
 
     if (!userId) {
-        return res.status(401).json({ success: false, message: 'user_id가 필요합니다.' });
+        return res.status(400).json({ success: false, message: 'user_id가 필요합니다.' });
     }
 
     // 콜백 기반 응답
     TwTODO(userId, (err, result) => {
         if (err) {
             console.error('DB 오류:', err);
-            return res.status(502).json({ success: false, message: '서버 오류', error: err });
+            return res.status(500).json({ success: false, message: '서버 오류', error: err });
         }
 
         return res.status(200).json({ success: true, data: result });
