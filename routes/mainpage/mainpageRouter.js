083f6ea149a4f54f4711a1eaf322ee39eb73e7b9
Dragons0up <jungyongguk5@gmail.com>
2025-08-04 03:21:38 +0900

    backend-5, 6, etc
    - 쿼리에서 경로 파라미터로 변경
    - 토큰 저장 추가
    - backend-5: 알림 읽음 처리 추가
    - backend-6: 임시로 알림 저장 기능만

--- routes/mainpage/mainpageRouter.js
+++ routes/mainpage/mainpageRouter.js
@@ -1,12 +1,16 @@
 const express = require('express');
 const router = express.Router();
 
-const { getNotices } = require('../../controllers/mainpage/noticeController');
+const { getNotices, updatePushToken, readNotice, sendNotice } = require('../../controllers/mainpage/noticeController');
 const { getTodayQuestion } = require('../../controllers/mainpage/mainpageController');
 const { Twcalendar } = require('../../controllers/mainpage/mainpageController');
 const { getRecentFeedback } = require('../../controllers/mainpage/mainpageController');
 
-router.get('/', getNotices);
+router.post('/push-token', updatePushToken);
+router.get('/:userId', getNotices);
+router.patch('/:userId/:noticeId/read', readNotice);
+router.patch('/:userId/read', readNotice);
+router.post('/send', sendNotice);
 router.get('/today', getTodayQuestion);  
 router.get('/thisweek', Twcalendar);  
 router.get('/recent', getRecentFeedback); 
