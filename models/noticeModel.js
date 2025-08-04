db6c875843c4f8f39d4022027a9373367d4273e8
Dragons0up <jungyongguk5@gmail.com>
2025-08-04 16:17:10 +0900

    backend-6: (알림 전송)
    - 푸시 토큰 저장
    - 알림 전송(저장만)

--- models/noticeModel.js
+++ models/noticeModel.js
@@ -13,6 +13,31 @@ const getUserNotices = (userId, callback) => {
     });
 };
 
+const updateUserPushToken = (userId, pushToken, callback) => {
+    const sql = `
+        UPDATE users
+        SET push_token = ?
+        WHERE id = ?
+    `;
+    db.query(sql, [pushToken, userId], (err, results) => {
+        if (err) return callback(err);
+        callback(null, results);
+    });
+}
+
+const insertNotice = (userId, title, content, callback) => {
+    const sql = `
+        INSERT INTO notice (title, content, users_id)
+        VALUES (?, ?, ?)
+    `;
+    db.query(sql, [title, content, userId], (err, results) => {
+        if (err) return callback(err);
+        callback(null, results);
+    });
+};
+
 module.exports = {
     getUserNotices,
+    updateUserPushToken,
+    insertNotice,
 };
--- routes/mainpage/mainpageRouter.js
+++ routes/mainpage/mainpageRouter.js
@@ -1,12 +1,14 @@
 const express = require('express');
 const router = express.Router();
 
-const { getNotices } = require('../../controllers/mainpage/noticeController');
+const { getNotices, updatePushToken, sendNotice } = require('../../controllers/mainpage/noticeController');
 const { getTodayQuestion } = require('../../controllers/mainpage/mainpageController');
 const { Twcalendar } = require('../../controllers/mainpage/mainpageController');
 const { getRecentFeedback } = require('../../controllers/mainpage/mainpageController');
 
 router.get('/', getNotices);
+router.post('/push-token', updatePushToken);
+router.post('/send', sendNotice);
 router.get('/today', getTodayQuestion);  
 router.get('/thisweek', Twcalendar);  
 router.get('/recent', getRecentFeedback); 
