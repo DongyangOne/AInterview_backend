73da0311f6125aadb20f940137a557e39f834b32
msh1234777 <msh1234777@naver.com>
2025-08-05 03:46:45 +0900

    커밋 메시지 작성

--- routes/feedback/feedbackRouter.js
+++ routes/feedback/feedbackRouter.js
@@ -4,8 +4,8 @@ const router = express.Router();
 const feedbackController = require('../../controllers/feedback/feedbackController');
 const { getPin, getUnpin } = require('../../controllers/feedback/feedbackController');
 
-router.get('/sort', feedbackController.sortFeedbacks);
-router.get('/search', feedbackController.searchFeedbacks);
+router.get('/:userId/sort', feedbackController.sortFeedbacks);
+router.get('/:userId/search', feedbackController.searchFeedbacks);
 router.get('/:userId', feedbackController.getAllFeedback);
 router.get('/:userId/:feedbackId/title', feedbackController.getFeedbackTitle);
 router.patch('/:userId/:feedbackId/title', feedbackController.updateFeedbackTitle);
@@ -13,7 +13,7 @@ router.delete('/:feedbackId/:userId', feedbackController.deleteFeedback);
 router.get('/:userId/:feedbackId', feedbackController.getFeedbackDetail);
 router.get('/:userId/:feedbackId/memo', feedbackController.getFeedbackMemo);
 router.patch('/:userId/:feedbackId/memo', feedbackController.updateFeedbackMemo);
-router.patch('/pin/:feedback_id', getPin);
-router.patch('/unpin/:feedback_id', getUnpin);
+router.patch('/pin/:feedback_id/:userId', feedbackController.getPin);
+router.patch('/unpin/:feedback_id/:userId', feedbackController.getUnpin);
 
 module.exports = router;
