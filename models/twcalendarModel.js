8e2cbe0e9580cce5bb297c6fe2b18ec8aa90fdc9
rlaghldbs <rlaghldbs2@naver.com>
2025-08-04 10:30:11 +0900

    수정된 backend3

--- models/twcalendarModel.js
+++ models/twcalendarModel.js
@@ -7,11 +7,7 @@ const TwTODO=(userId,callback)=>{
 
 
     const sql=
-<<<<<<< HEAD
-    'SELECT *FROM calendar WHERE  DATE(time) BETWEEN DATE_SUB(CURRENT_DATE(), INTERVAL WEEKDAY(CURRENT_DATE()) DAY) AND DATE_ADD(CURRENT_DATE(), INTERVAL (6 - WEEKDAY(CURRENT_DATE())) DAY) AND users_id = ?;'
-=======
     'SELECT calendar_id, title FROM calendar WHERE  DATE(time) BETWEEN DATE_SUB(CURRENT_DATE(), INTERVAL WEEKDAY(CURRENT_DATE()) DAY) AND DATE_ADD(CURRENT_DATE(), INTERVAL (6 - WEEKDAY(CURRENT_DATE())) DAY) AND users_id = ?;'
->>>>>>> 9c2e91bb333fc0a122ae57eabf324d215d3307f5
 
      db.query(sql,[userId],(err,result)=>{
           if(err){
