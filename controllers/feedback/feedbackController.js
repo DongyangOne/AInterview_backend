7746b95c313d27cdd422fd16a43d9d3b9f23584b
chldPDms <115327796+chldPDms@users.noreply.github.com>
2025-08-05 11:15:52 +0900

    컨트롤러 수정

--- controllers/feedback/feedbackController.js
+++ controllers/feedback/feedbackController.js
@@ -275,7 +275,10 @@ if (!userId || !feedbackId) {
     if (err) {
       return res.status(500).json({ success: false, message: '서버 오류', error: err.message });
     }
-  
+
+    if (!feedback) {
+      return res.status(404).json({ success: false, message: '해당 피드백을 찾을 수 없습니다.' });
+    }
 
     res.status(200).json({
       success: true,
