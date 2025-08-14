eab76c63efc7313f0f6f177e1c71ba5a7f77d6ad
Dragons0up <jungyongguk5@gmail.com>
2025-08-14 20:20:09 +0900

    backend-6 경로 변경

--- models/notice/noticeModel.js
+++ models/notice/noticeModel.js
@@ -37,7 +37,32 @@ const markNoticeRead = (userId, noticeId, callback) => {
     });
 };
 
+const updateUserPushToken = (userId, pushToken, callback) => {
+    const sql = `
+        UPDATE users
+        SET push_token = ?
+        WHERE id = ?
+    `;
+    db.query(sql, [pushToken, userId], (err, results) => {
+        if (err) return callback(err);
+        callback(null, results);
+    });
+}
+
+const insertNotice = (userId, title, content, callback) => {
+    const sql = `
+        INSERT INTO notice (title, content, users_id)
+        VALUES (?, ?, ?)
+    `;
+    db.query(sql, [title, content, userId], (err, results) => {
+        if (err) return callback(err);
+        callback(null, results);
+    });
+};
+
 module.exports = {
     getUserNotices,
-    markNoticeRead
+    markNoticeRead,
+    updateUserPushToken,
+    insertNotice
 }
