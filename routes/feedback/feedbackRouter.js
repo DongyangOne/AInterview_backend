fada7a6f6a6d4d6abd2c7142238085c8c8cb7a55
msh1234777 <msh1234777@naver.com>
2025-08-15 23:21:17 +0900

    backend-12 라우터 memo 수정

--- routes/feedback/feedbackRouter.js
+++ routes/feedback/feedbackRouter.js
@@ -12,8 +12,6 @@ router.get('/:userId/:feedbackId/title', feedbackController.getFeedbackTitle);
 router.patch('/:userId/:feedbackId/title', feedbackController.updateFeedbackTitle);
 router.delete('/:feedbackId/:userId', feedbackController.deleteFeedback);
 router.get('/:userId/:feedbackId', feedbackController.getFeedbackDetail);
-router.get('/:userId/:feedbackId/memo', feedbackController.getFeedbackMemo);
-router.patch('/:userId/:feedbackId/memo', feedbackController.updateFeedbackMemo);
 router.patch('/pin/:feedbackId/:userId', feedbackController.getPin);
 router.patch('/unpin/:feedbackId/:userId', feedbackController.getUnpin);
 router.get('/:userId', feedbackController.getAllFeedback);
