4f2f19fb89c6a7054a728d579beacf7c90b9045c
Dragons0up <jungyongguk5@gmail.com>
2025-08-24 03:53:00 +0900

    swagger-0 스웨거 문서 세팅
    - 스웨거 문서 세팅 및 추가
    - 여러개의 yaml을 하나의 스웨거 문서로 병합하여 배포하는 방식을 가짐

--- server.js
+++ server.js
@@ -1,21 +1,23 @@
 require('dotenv').config();
 const express = require('express');
+const swaggerUi = require('swagger-ui-express');
+const YAML = require('yamljs');
+const path = require('path');
+
 const app = express();
 
+const swaggerDocument = YAML.load(path.join(__dirname, './swagger.yaml'));
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
+
 app.use(express.json());
-const db = require('./config/database');
+
 
 const authRouter = require('./routes/auth/authRouter');
-
 const calendarRouter  = require('./routes/calendar/calendarRouter');
-
-const mainpageRouter = require('./routes/mainpage/mainpageRouter');
-
-const noticeRouter = require('./routes/notice/noticeRouter');
-
 const feedbackRouter = require('./routes/feedback/feedbackRouter'); 
-
+const mainpageRouter = require('./routes/mainpage/mainpageRouter');
 const mypageRouter = require('./routes/mypage/mypageRouter');
+const noticeRouter = require('./routes/notice/noticeRouter');
 
 
 app.use('/sign', authRouter);
@@ -23,16 +25,19 @@ app.use('/logout', authRouter);
 
 app.use('/calendar', calendarRouter);
 
-app.use('/mainpage', mainpageRouter);
-app.use('/notice', noticeRouter);
-
 app.use('/feedback', feedbackRouter); 
+
+app.use('/mainpage', mainpageRouter);
   
 app.use('/myPage', mypageRouter);
 app.use('/user', mypageRouter);
 app.use('/delete', mypageRouter);
 
+app.use('/notice', noticeRouter);
+
+
 const port = process.env.s_port || 3000;
 app.listen(port, '0.0.0.0', () => {
-  console.log(`서버 시작 ${port}`);
+  console.log(`\n서버 시작: http://localhost:${port}`);
+  console.log(`Swagger UI: http://localhost:${port}/api-docs`);
 });
